494af4eed5921693121c6b0150b22c8a53927234
12238 <xmjhf@intretech.com>
2019-09-18 10:18:02 +0800

    whitford apitest project

---
+++ APIView/APIView-ok/APIView/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APIView.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "jhf test";
+
+            return View();
+        }
+
+        public ActionResult Top()
+        {
+            ViewBag.UserName = "超级管理员";
+            ViewBag.AvailableBalance = "8888.00";
+            return View();
+        }
+        public ActionResult Left()
+        {
+            return View();
+        }
+        public ActionResult Right()
+        {
+            return View();
+        }
+
+        public ActionResult Bootom()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ APIView/APIView-ok/APIView/Models/ChannelInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIView.Models
+{
+    public class ChannelInfo
+    {
+        public virtual int ID { get; set; }
+
+        public virtual string ChannelStyle { get; set; }
+
+        public virtual string ChannelCode { get; set; }
+        public virtual string CnName { get; set; }
+        public virtual string EnName { get; set; }
+
+        public virtual string Status { get; set; }
+
+    }
+}
